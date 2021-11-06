f4339066b72f5049c212af8d987687265468d8d6
Gili Yaniv <gili@lumigo.io>
2021-11-06 15:05:48 +0200

    docs: ✏️ improve docs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ScrollManagerDirectiveModule } from '../../projects/scroll-manager/src/lib/scroll-manager.directive';
+import { ScrollManagerDirectiveModule } from '@ritox/scroll-manager';
 
 @NgModule({
   declarations: [AppComponent],
